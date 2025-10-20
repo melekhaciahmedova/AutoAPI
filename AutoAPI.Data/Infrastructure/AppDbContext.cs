db36469516002eb821031bc5c4011f2140b9fb6e
Melek.H <melek.h@siesco.az>
2025-10-20 19:04:26 +0400

    .

--- AutoAPI.Data/Infrastructure/AppDbContext.cs
+++ AutoAPI.Data/Infrastructure/AppDbContext.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 
 namespace AutoAPI.Data.Infrastructure
 {
-    public partial class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
